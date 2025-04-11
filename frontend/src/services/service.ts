9c8346eb7bca3971acbbf05b3493e478ccc9b874
Jay-Kunaico <jay.brass@kunaico.com>
2025-04-11 15:29:12 -0400

    added log

--- frontend/src/services/service.ts
+++ frontend/src/services/service.ts
@@ -1,6 +1,7 @@
 import type { ResultType } from "../types/types";
 
 const BASE_URL = import.meta.env.VITE_API_URL;
+console.log("BASE_URL:", BASE_URL);
 
 export const fetchData = async (url: string) => {
 	try {
