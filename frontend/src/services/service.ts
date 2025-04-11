e3753f016a8d142fbdf86f77c6b0ef8afbd96704
Jay-Kunaico <jay.brass@kunaico.com>
2025-04-11 13:30:32 -0400

    path changes

--- frontend/src/services/service.ts
+++ frontend/src/services/service.ts
@@ -1,6 +1,6 @@
 import type { ResultType } from "../types/types";
 
-const BASE_URL = 'http://127.0.0.1:5000';
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 export const fetchData = async (url: string) => {
 	try {
